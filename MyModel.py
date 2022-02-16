b9fb7d9871f7e8175f0cd5553e96c57b64540b2b
joyGit <joy_sifa@yahoo.com>
2022-02-16 20:53:57 +0600

    Practice database functionality

--- MyModel.py
+++ MyModel.py
@@ -11,15 +11,17 @@ def main():
     dbms = MyDatabase.MyDatabase(MyDatabase.SQLITE, dbname='mydb.sqlite')
 
     # Create Tables
-    dbms.create_db_tables()
+    # dbms.create_db_tables()
+
     # # dbms.insert_single_data()
+
     # dbms.print_all_data(MyDatabase.USERS)
     # dbms.print_all_data(MyDatabase.ADDRESSES)
-    #
-    # dbms.sample_query() # simple query
-    # dbms.sample_delete() # delete data
-    # dbms.sample_insert() # insert data
-    # dbms.sample_update() # update data
+
+    dbms.sample_query()   # simple query
+    dbms.sample_delete()  # delete data
+    dbms.sample_insert()  # insert data
+    dbms.sample_update()  # update data
 
 
 # run the program
